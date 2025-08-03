b8424e75bbf7c729e711ea55925e30971707ed0e
Julien <nifrael66@gmail.com>
2025-08-03 17:48:48 +0200

    Prix des maquillages et soins

---
+++ src/data/maquillagePrices.ts
@@ -0,0 +1,32 @@
+export const maquillagePrices = [
+    {
+  name: "Maquillage jour",
+  duration: "40 min",
+  price: "30€",
+  description: "Teint frais, lumineux et naturel, regard léger, idéal pour la journée ou un entretien.",
+    },
+    {
+    name: "Maquillage Soir",
+    duration: "60 min",
+    price: "45€",
+    description: "Teint travaillé, regard intensifié, plus de sophistication et de tenue.",
+    },
+    {
+    name: "Maquillage Mariée",
+    duration: "90 min",
+    price: "90€",
+    description: "Préparation complète, teint longue tenue, harmonie avec la tenue.",
+    },
+    {
+    name: "Essai Maquillage Mariée",
+    duration: "60 min",
+    price: "50€",
+    description: "Essai préalable avec ajustement personnalisé.",
+    },
+    {
+    name: "Maquillages Convives",
+    duration: "40 min",
+    price: "35€",
+    description: "Mise en beauté pour les invitées du mariage ou évènements.",
+    }
+]

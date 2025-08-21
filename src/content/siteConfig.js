ad50537e49184e876f5874675a057ace1c18814b
Julien <julien.dev66@gmail.com>
2025-08-21 23:18:01 +0200

    seo allégé

--- src/content/siteConfig.js
+++ src/content/siteConfig.js
@@ -1,20 +1,6 @@
 export const businessInfo = {
   "@context": "https://schema.org",
-  "@type": "HealthAndBeautyBusiness",
-  "name": "L'Instant Soleana - Esthétique et bien-être à domicile",
-  "description": "Soledad, esthéticienne diplômée, vous propose des massages, soins, épilations et maquillages à domicile à Perpignan et ses environs.",
-  "telephone": "+33625487861",
-  "image": "https://www.linstant-soleana.fr/favicon.svg",
+  "@type": "WebSite",
+  "name": "L'Instant Soleana",
   "url": "https://www.linstant-soleana.fr/",
-  "@id": "https://www.linstant-soleana.fr/#organization",
-  "areaServed": {
-    "@type": "City",
-    "name": "Perpignan"
-  },
-  "priceRange": "€€",
-    "geo": {
-    "@type": "GeoCoordinates",
-    "latitude": "42.6988",
-    "longitude": "2.8952"
-  }
 };

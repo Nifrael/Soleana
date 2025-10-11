5c9ff1d9b7be2762f3767b839b44ac342e0cfe4b
Julien <julien.dev66@gmail.com>
2025-10-11 12:42:31 +0200

    Composant SEO

--- src/content/siteConfig.js
+++ src/content/siteConfig.js
@@ -4,3 +4,10 @@ export const businessInfo = {
   "name": "L'Instant Soleana",
   "url": "https://www.linstant-soleana.fr/",
 };
+
+const siteConfig = {
+  title: "L'Instant Soleana - Près de Perpignan",
+  description: "Découvrez L'Instant Soleana, offrez-vous une parenthèse de bien-être pour des massages relaxants, des soins du visage et du corps, des épilations et maquillages professionnels.",
+};
+
+export default siteConfig;

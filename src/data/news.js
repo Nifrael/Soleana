8ecc914cc3808b14268cb27facffce33dde606b3
Julien <julien.dev66@gmail.com>
2026-06-08 23:02:18 +0200

    Nouveautés : lien de prise de rendez-vous

--- src/data/news.js
+++ src/data/news.js
@@ -1,6 +1,18 @@
 import jeannelleImage from "../assets/images/Jeannelle.jpg";
+import Calendar from "../assets/images/Calendrier.jpg";
 
 export const newsData = [
+  {
+    title:
+      "Réservez votre moment de douceur parmi une sélection de prestations",
+    date: "",
+    description: "Retrouvez-moi à Millas, les samedis, sur rendez-vous.",
+    image: Calendar,
+    imageAlt: "Calendrier",
+    isNew: true,
+    link: "https://www.sumupbookings.com/linstant-soleana",
+    linkText: "Réservez",
+  },
   {
     title: "Retrouvez L'Instant Soleana chez Jeannelle",
     date: "",
